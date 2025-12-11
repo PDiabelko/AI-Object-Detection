4d6413edf9cd1d0cfa37c0acbcb6239295445074
James Homan <99617985+James-Homan@users.noreply.github.com>
2025-12-11 18:58:49 -0500

    Created New App to demo.

---
+++ crosswalk.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Crosswalk Detection App",
+        page_icon="🚸",
+        layout="wide"
+    )
+    st.title("Crosswalk Detection App")
+    
+
+if __name__ == "__main__":
+    main()
